1e15b6691022d619db625d96c2a4218acadcba15
Victor Antonio Ruiz Chambi <vruizc@proveedor.intercorp.com.pe>
2022-05-08 22:39:00 -0500

    trabajando con variables de entorno en node.js

---
+++ practica01.js
@@ -0,0 +1,10 @@
+let nombre = process.env.NOMBRE || 'Sin nombre';
+let web = process.env.MI_WEB || 'no tengo web';
+
+// para que salga valor se tiene que ejecutar el siguiente comando
+// NOMBRE=Victor node practica01.js  
+console.log('Hola '+ nombre);
+
+// para que salga valor se tiene que ejecutar el siguiente comando
+// NOMBRE=Victor MI_WEB=vitocho.com node practica01.js 
+console.log('Mi web es '+ web);
