ba7e37fe802044ff831629135b682ce002065c03
Victor Antonio Ruiz Chambi <varuizchambi@gmail.com>
2022-05-20 13:22:31 -0500

    se realizan cambios para hacer pruebas de comunicación con repositorio remoto

--- practica01.js
+++ practica01.js
@@ -8,3 +8,4 @@ console.log('Hola '+ nombre);
 // para que salga valor se tiene que ejecutar el siguiente comando
 // NOMBRE=Victor MI_WEB=vitocho.com node practica01.js 
 console.log('Mi web es '+ web);
+console.log('Prueba para commit de proyecto:' + web)
